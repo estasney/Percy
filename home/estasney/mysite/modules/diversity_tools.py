a7e6c45904a6c6a89211599d4205b4017c7a6848
estasney <estasney@users.noreply.github.com>
2017-12-19 12:53:09 -0500

    Added generic Genderize class to handle querying extensible data sources and models, returning in parseable format

--- home/estasney/mysite/modules/diversity_tools.py
+++ home/estasney/mysite/modules/diversity_tools.py
@@ -1,5 +1,6 @@
 from operator import itemgetter
-import queue
+from collections import namedtuple
+
 
 
 # user_form = self.request_object
@@ -30,157 +31,171 @@ import queue
     # If exclude ML
         # Do stats
 
-class DataFetcher(object):
+class Genderize(object):
 
-    def __init__(self, query, *data_file):
-        self.query = self.format_query_(query)
-        self.data_file = [d for d in data_file]
-        self.query_results = {'male': 0, 'female': 0, 'ambiguous': 0, 'unknown': []}
+    GenderResults = namedtuple('GenderResults', 'query source result')
 
-    def format_query_(self, query):
-        if isinstance(query, str):
-            query = [query]
-        elif isinstance(query, list) is False:
-            raise ValueError
-        fl = [q.lower() for q in query]
-        query_q = queue.Queue()
-        for qq in fl:
-            query_q.put_nowait(qq)
-        return query_q
+    def __init__(self, **resources):
+        """
+        :param resources: Provide resources that class may access to determine gender.
+            :keyword - in form (data) - (int) : for database, (model) - (int) : for model
+            :value - object
+        """
+        self.datas = {}
+        self.models = {}
+        if resources:
+            for k, v in resources.items():
+                if "data" in k:
+                    self.datas[k] = v
+                elif "model" in k:
+                    self.models[k] = v
 
+    def gender_features_(self, query):
+        name = query.lower()
+        features = {}
+        features['first_two'] = name[:2]
+        features['last_letter'] = name[-1]
+        features['last_letter_vowel'] = self.vowel_test_(name[-1])
+        features['last_two'] = name[-2:]
+        return features
 
-    def query_name_(self, name, data_file_index=0):
-        if name not in self.data_file[data_file_index]:
-            return False
+    def vowel_test_(self, letter):
+        vowels = ["a", "e", "i", "o", "u", "y"]
+        if letter in vowels:
+            return "Yes"
         else:
-            return self.data_file[data_file_index][name]
+            return "No"
 
-    def parse_value_(self, value):
-        if isinstance(value, dict): # SSA Database
-            sorted_data = sorted(value, key=itemgetter(1), reverse=True)
-            top_gender = sorted_data[0][0]
-            return top_gender
-        if isinstance(value, str): # Global Name Database
-            return value
-
-    def run_query(self):
-        for q in self.query:
-            self.query_name_(q)
-
-def worker():
-    while True:
-        item = q.get()
-        if item is None:
-            break
-        do_work(item)
-        q.task_done()
-
-q = queue.Queue()
-threads = []
-for i in range(num_worker_threads):
-    t = threading.Thread(target=worker)
-    t.start()
-    threads.append(t)
-
-for item in source():
-    q.put(item)
-
-# block until all tasks are done
-q.join()
-
-# stop workers
-for i in range(num_worker_threads):
-    q.put(None)
-for t in threads:
-    t.join()
-
-
-
-
-
-
-
-
-
-
-def gender_features(name):
-    name = name.lower()
-    features = {}
-    features['first_two'] = name[:2]
-    features['last_letter'] = name[-1]
-    features['last_letter_vowel'] = vowel_test(name[-1])
-    features['last_two'] = name[-2:]
-    return features
-
-
-def vowel_test(letter):
-    vowels = ["a", "e", "i", "o", "u", "y"]
-    if letter in vowels:
-        return "Yes"
-    else:
-        return "No"
-
-def retrieve_names_bulk(name_list, name_dict, global_dict=None, infer_gender=True):
-    male_count = 0
-    female_count = 0
-    unknown_count = 0
-    amb_count = 0 # Ambiguous count
-    ai_count = 0
-    unknown_holder = []
-    ai_dict = {}
-
-    for name in name_list:
-        if name not in name_dict:
-            unknown_holder.append(name)
-            continue
-        gender_lookup = retrieve_name(name, name_dict)
-        top_gender = parse_ssa_lookup(gender_lookup)
-        if top_gender == 'M':
-            male_count += 1
-        elif top_gender == 'F':
-            female_count += 1
-
-    if global_dict and unknown_holder:
-        for name in unknown_holder:
-            if name not in global_dict:
-                continue
-            gender_lookup = retrieve_name(name, global_dict)
-            top_gender = gender_lookup
-            if top_gender == 'M':
-                male_count += 1
-            elif gender_lookup == 'F':
-                female_count += 1
+    def parse_result_(self, result):
+        if isinstance(result, dict): # SSA Format {'M': int, 'F': int}
+            # ensure not equal
+            if list(result.values())[0] == list(result.values())[1]:
+                return 'U' # U for ambiguous
+            #  return the top scoring gender
+            return sorted(result.items(), key=itemgetter(1), reverse=True)[0][0]
+        if isinstance(result, str): # Global Names Format or Classifier
+            if len(result) == 1:
+                return result.upper()
             else:
-                amb_count += 1
-            unknown_holder.pop(unknown_holder.index(name))
+                return result[0].upper()
 
-    if infer_gender is False:
-        diversity_score_dict = {'male': male_count, 'female': female_count, 'unknown': len(unknown_holder),
-                                'amb_count': amb_count, 'ai_names': False, 'ai_count': 0}
-        return diversity_score_dict
+    def format_result_(self, query, source, result):
+        res = self.GenderResults(query=query, source=source, result=result)
+        return res
 
-    for name in unknown_holder:
-        inferred_gender = guess_name(name).lower()
-        if inferred_gender == 'male':
-            male_count += 1
-            ai_dict[name.title()] = 'Male'
-        elif inferred_gender == 'female':
-            female_count = female_count + 1
-            ai_dict[name.title()] = 'Female'
-        ai_count += 1
+    def data_lookup_(self, query):
+        query_l = query.lower()
 
-    diversity_score_dict = {'male': male_count, 'female': female_count, 'unknown': 0, 'amb_count': 0,
-                            'ai_names':ai_dict, 'ai_count': ai_count}
-    return diversity_score_dict
+        if self.datas:
+            for data_name, data_file in self.datas.items():
+                if query_l in data_file:
+                    parsed_finding = self.parse_result_(data_file[query_l])
+                    format_finding = self.format_result_(query, data_name, parsed_finding)
+                    return format_finding
+        else:
+            return False
+
+    def ask_model_(self, query):
+        query_l = query.lower()
+        if self.models:
+            query_features = self.gender_features_(query_l)
+            for model_name, model_obj in self.models.items():
+                guess = model_obj.classify(query_features)
+                parsed_guess = self.parse_result_(guess)
+                format_finding = self.format_result_(query, model_name, parsed_guess)
+                return format_finding
+        else:
+            return False
+
+    def run_query(self, query):
+        results = []
+        data_results = self.data_lookup_(query)
+        if data_results:
+            results.append(data_results)
+        model_results = self.ask_model_(query)
+        results.append(model_results)
+        return results
 
 
 
-def guess_name(name):
-    inferred_gender = tree_model.classify(gender_features(name)).title()
-    return inferred_gender
 
-user_query_name = request.form['infer_name']
-        inferred_gender = tree_model.classify(gender_features(user_query_name)).title()
-        gender_lookup = retrieve_name(user_query_name, name_dict)[1]  # Selecting the message
-        return render_template('infer.html', user_query=user_query_name, success='True', gender_guess=inferred_gender,
-                               lookup_message=gender_lookup)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def retrieve_names_bulk(name_list, name_dict, global_dict=None, infer_gender=True):
+#     male_count = 0
+#     female_count = 0
+#     unknown_count = 0
+#     amb_count = 0 # Ambiguous count
+#     ai_count = 0
+#     unknown_holder = []
+#     ai_dict = {}
+#
+#     for name in name_list:
+#         if name not in name_dict:
+#             unknown_holder.append(name)
+#             continue
+#         gender_lookup = retrieve_name(name, name_dict)
+#         top_gender = parse_ssa_lookup(gender_lookup)
+#         if top_gender == 'M':
+#             male_count += 1
+#         elif top_gender == 'F':
+#             female_count += 1
+#
+#     if global_dict and unknown_holder:
+#         for name in unknown_holder:
+#             if name not in global_dict:
+#                 continue
+#             gender_lookup = retrieve_name(name, global_dict)
+#             top_gender = gender_lookup
+#             if top_gender == 'M':
+#                 male_count += 1
+#             elif gender_lookup == 'F':
+#                 female_count += 1
+#             else:
+#                 amb_count += 1
+#             unknown_holder.pop(unknown_holder.index(name))
+#
+#     if infer_gender is False:
+#         diversity_score_dict = {'male': male_count, 'female': female_count, 'unknown': len(unknown_holder),
+#                                 'amb_count': amb_count, 'ai_names': False, 'ai_count': 0}
+#         return diversity_score_dict
+#
+#     for name in unknown_holder:
+#         inferred_gender = guess_name(name).lower()
+#         if inferred_gender == 'male':
+#             male_count += 1
+#             ai_dict[name.title()] = 'Male'
+#         elif inferred_gender == 'female':
+#             female_count = female_count + 1
+#             ai_dict[name.title()] = 'Female'
+#         ai_count += 1
+#
+#     diversity_score_dict = {'male': male_count, 'female': female_count, 'unknown': 0, 'amb_count': 0,
+#                             'ai_names':ai_dict, 'ai_count': ai_count}
+#     return diversity_score_dict
+#
+#
+#
+# def guess_name(name):
+#     inferred_gender = tree_model.classify(gender_features(name)).title()
+#     return inferred_gender
+#
+# user_query_name = request.form['infer_name']
+#         inferred_gender = tree_model.classify(gender_features(user_query_name)).title()
+#         gender_lookup = retrieve_name(user_query_name, name_dict)[1]  # Selecting the message
+#         return render_template('infer.html', user_query=user_query_name, success='True', gender_guess=inferred_gender,
+#                                lookup_message=gender_lookup)
