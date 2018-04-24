1f9577333cfa178fe389de696aaabb99c8114f23
Eric Stasney <ericstasney@gmail.com>
2018-04-23 23:02:38 -0400

    update site config

--- mysite/app_folder/__init__.py
+++ mysite/app_folder/__init__.py
@@ -1,16 +1,8 @@
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
-try:
-    from app_folder.local_config import Config
-except ImportError:
-    from app_folder.web_config import Config
+from app_folder.site_config import FConfig
 
 app_run = Flask(__name__)
-app_run.config['UPLOAD_FOLDER'] = Config.UPLOAD_FOLDER
-app_run.config['DEBUG'] = Config.debug
-app_run.config['SECRET_KEY'] = Config.secret_key
-app_run.config['DEBUG_TB_PROFILER_ENABLED'] = Config.profiler
-
-toolbar = DebugToolbarExtension(app_run)
+app_run.config['UPLOAD_FOLDER'] = FConfig.UPLOAD_FOLDER
 
 from app_folder import routes
