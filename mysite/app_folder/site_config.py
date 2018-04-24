0a4228034f3fe9ee3b86a4c20cdfe7a93e125c03
Eric Stasney <ericstasney@gmail.com>
2018-04-23 23:06:32 -0400

    fix path

--- mysite/app_folder/site_config.py
+++ mysite/app_folder/site_config.py
@@ -3,28 +3,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class FConfig(object):
-    model = os.path.join(basedir, 'app_folder/mymodel.model')
-    gram_path = os.path.join(basedir, 'app_folder/trigram_model.p')
-    tree = os.path.join(basedir, 'app_folder/tree_classifier.pickle')
-    name_dict = os.path.join(basedir, 'app_folder/name_dict.pickle')
-    global_name_dict = os.path.join(basedir, 'app_folder/global_name_dict.pickle')
-    name_file_path = os.path.join(basedir, 'app_folder/name_list.csv')
-    raw_dict = os.path.join(basedir, 'app_folder/tfidf_dict.dict')
-    tfidf_model = os.path.join(basedir, 'app_folder/tfidf.model')
-    bigram_dict_path = os.path.join(basedir, 'app_folder/bigram_tfidf_dict.dict')
-    bigram_tfidf_model_path = os.path.join(basedir, 'app_folder/bigram_tfidf.model')
-    lem_dict_path = os.path.join(basedir, 'app_folder/l_dictionary.dict')
-    lem_tfidf_model_path = os.path.join(basedir, 'app_folder/l_tfidf.model')
-    lg_dict_path = os.path.join(basedir, 'app_folder/lg_dictionary.dict')
-    lg_tfidf_model_path = os.path.join(basedir, 'app_folder/lg_tfidf.model')
-    UPLOAD_FOLDER = os.path.join(basedir, 'app_folder/uploads')
-
-
-
-
-
-
-
-
-
+    model = os.path.join(basedir, 'resources/mymodel.model')
+    gram_path = os.path.join(basedir, 'resources/trigram_model.p')
+    tree = os.path.join(basedir, 'resources/tree_classifier.pickle')
+    name_dict = os.path.join(basedir, 'resources/name_dict.pickle')
+    global_name_dict = os.path.join(basedir, 'resources/global_name_dict.pickle')
+    name_file_path = os.path.join(basedir, 'resources/name_list.csv')
+    raw_dict = os.path.join(basedir, 'resources/tfidf_dict.dict')
+    tfidf_model = os.path.join(basedir, 'resources/tfidf.model')
+    bigram_dict_path = os.path.join(basedir, 'resources/bigram_tfidf_dict.dict')
+    bigram_tfidf_model_path = os.path.join(basedir, 'resources/bigram_tfidf.model')
+    lem_dict_path = os.path.join(basedir, 'resources/l_dictionary.dict')
+    lem_tfidf_model_path = os.path.join(basedir, 'resources/l_tfidf.model')
+    lg_dict_path = os.path.join(basedir, 'resources/lg_dictionary.dict')
+    lg_tfidf_model_path = os.path.join(basedir, 'resources/lg_tfidf.model')
+    UPLOAD_FOLDER = os.path.join(basedir, 'resources/uploads')
 
