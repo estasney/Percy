8dbe64bff140c19549cd7fef8e465bdd10029c12
estasney <16440579+estasney@users.noreply.github.com>
2018-08-09 00:11:31 -0400

    add vec

--- mysite/app_folder/main/neural_tools.py
+++ mysite/app_folder/main/neural_tools.py
@@ -1,50 +1,64 @@
-from app_folder import model
+
+import numpy as np
+from gensim.corpora import Dictionary
 from collections import OrderedDict
 from operator import itemgetter
-
-def prettify_dict(d):
-    p_dict = OrderedDict()
-    for k, v in d.items():
-        pk = k.replace("_", " ")
-        pk = pk.title()
-        pv = "{:.2%}".format(v)
-        p_dict[pk] = pv
-    return p_dict
-
-def load_model():
-    return model
-
-
-def word_sims(user_query, prettify=True, topn=50):
-    model = load_model()
-    try:
-        result = model.similar_by_word(user_query, topn=topn)
-        result = sorted(result, key=itemgetter(1), reverse=True)
-        result = OrderedDict(result)
-    except KeyError as error:
-        return False, error
-    if prettify:
-        result = prettify_dict(result)
-    return True, result
-
+from app_folder.site_config import FConfig
 
 def word_math(request):
-    word_one = request.form['word1'].lower()
-    word_two = request.form['word2'].lower()
-    word_three = request.form['word3'].lower()
-    model = load_model()
-    try:
-        if word_three == '':
-            user_equation = word_one.title() + " + " + word_two.title() + " = "
-            result = dict(model.most_similar(positive=[word_one, word_two]))
-        else:
-            user_equation = word_one.title() + " + " + word_two.title() + " - " + word_three.title() + " = "
-            result = dict(model.most_similar(positive=[word_one, word_two], negative=[word_three]))
+    pwords = request.form.get('pwords').split(",")
+    pwords = [x.replace(" ", "_").lower() for x in pwords]
+    neg_words = request.form.get('neg_words')
+    if neg_words:
+        neg_words = neg_words.split(",")
+        neg_words = [x.replace(" ", "_").lower() for x in neg_words]
+    ws = WordSims()
+    vec = ws.word_math_vec_(pwords, neg_words)
+    sims = ws.find_similar_vec(vec)
+    return sims
 
-        return {'result': prettify_dict(result), 'success': True, 'user_equation':user_equation, 'word_one':word_one.strip(), 'word_two':word_two.strip(),
-                'word_three': word_three.strip()}
-    except KeyError as error:
-        error_message = str(error)
-        offending_term = error_message.split("'")[1]
-        result = offending_term.title()
-        return {'result': result, 'success': False}
+
+
+
+class WordSims(object):
+
+    def __init__(self):
+        self.array = np.load(FConfig.lda_pmi)
+        self.idx = Dictionary.load(FConfig.dictionary)
+
+    def find_similar(self, word):
+        word = word.replace(" ", "_")
+        try:
+            word_id = self.idx.token2id[word]
+        except KeyError:
+            return None
+
+        dd = np.dot(self.array, self.array[word_id])  # Cosine similarity for this unigram against all others.
+        sims = np.argsort(-1 * dd)[:100]
+        sim_scores = [(self.idx.get(sim), dd[sim]) for sim in sims if word_id != sim]
+
+        # Remove None Id2Word
+        sim_scores = list(filter(lambda x: x[0] is not None, sim_scores))
+        sim_scores.sort(key=lambda x: x[1], reverse=True)
+        return sim_scores
+
+    def find_similar_vec(self, vec):
+
+        dd = np.dot(self.array, vec)  # Cosine similarity for this unigram against all others.
+        sims = np.argsort(-1 * dd)[:100]
+        sim_scores = [(self.idx.get(sim), dd[sim]) for sim in sims]
+
+        # Remove None Id2Word
+        sim_scores = list(filter(lambda x: x[0] is not None, sim_scores))
+        sim_scores.sort(key=lambda x: x[1], reverse=True)
+        return sim_scores
+
+    def word_math_vec_(self, positive_words, negative_words=None):
+        positive_words_ids = [self.idx.token2id.get(w) for w in positive_words]
+        positive_word_vec = np.sum([self.array[x] for x in positive_words_ids], axis=0)
+        if not negative_words:
+            return positive_word_vec
+        negative_words_ids = [self.idx.token2id.get(w) for w in negative_words]
+        negative_word_vec = np.sum([self.array[x] for x in negative_words_ids], axis=0)
+        result_vec = positive_word_vec - negative_word_vec
+        return result_vec
