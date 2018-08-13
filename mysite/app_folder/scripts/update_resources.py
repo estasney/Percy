d558bc2442f0f1ff9a0b04100b6df3f17ea9d504
estasney <16440579+estasney@users.noreply.github.com>
2018-08-13 14:32:29 -0400

    wpi script

--- mysite/app_folder/scripts/update_resources.py
+++ mysite/app_folder/scripts/update_resources.py
@@ -1,16 +1,23 @@
+import logging
+import multiprocessing
+import pickle
+import string
+from collections import Counter
+from datetime import datetime
+from itertools import combinations
+from math import log
+
+import numpy as np
 import pandas as pd
 from gensim.corpora.dictionary import Dictionary
+from gensim.models.phrases import Phrases, Phraser
 from gensim.utils import strided_windows
-from itertools import combinations
-import logging
-from collections import Counter
-import numpy as np
-from app_folder.main.text_tools import STOPWORDS
-import string
+from langdetect import detect
 from scipy.sparse import csc_matrix
-from math import log
 from scipy.sparse.linalg import svds
-from datetime import datetime
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from app_folder.main.text_tools import STOPWORDS
 
 """
 Resources to update
@@ -26,16 +33,42 @@ Words
 
 """
 
-df = pd.read_csv(r"/home/eric/PycharmProjects/FlaskAPI/scripts2/corpus.csv")
-df.dropna(subset=['summary'], inplace=True)
-docs = df['summary'].values.tolist()
-del df
+
+def _apply_df(args):
+    df, func, num, kwargs = args
+    return num, df.apply(func, **kwargs)
+
+
+def apply_by_multiprocessing(df, func, **kwargs):
+    workers = kwargs.pop('workers')
+    pool = multiprocessing.Pool(processes=workers)
+    result = pool.map(_apply_df, [(d, func, i, kwargs) for i, d in enumerate(np.array_split(df, workers))])
+    pool.close()
+    result = sorted(result, key=lambda x: x[0])
+    return pd.concat([i[1] for i in result])
+
+
+def lang_detect(x):
+    return detect(x['summary'])
+
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(r"/home/eric/PycharmProjects/FlaskAPI/scripts2/corpus.csv")
+    df.dropna(subset=['summary'], inplace=True)
+    start = datetime.now()
+    print("Starting Language Detection")
+    df['lang'] = apply_by_multiprocessing(df, lang_detect, axis=1, workers=8)
+    elapsed = datetime.now() - start
+    print("Finished Language Detection in {} seconds".format(elapsed.seconds))
+
+df = df.loc[df['lang'] == 'en']
 
 def preprocess_text(x, preserve_sent=False, stopwords=STOPWORDS):
     from pattern.en import parsetree
 
     punc = set(string.punctuation)
-    tree = parsetree(x, tokenize=True, lemmata=True)
+    tree = parsetree(x['summary'], tokenize=True, lemmata=True)
     doc = [sent.lemma for sent in tree]
     if preserve_sent is False:
         pdoc = [word for sent in doc for word in sent]
@@ -55,8 +88,10 @@ DICTIONARY - Words
 """
 print("Building Dictionary")
 start = datetime.now()
-pdocs = [preprocess_text(doc, preserve_sent=False) for doc in docs]
-del docs
+if __name__ == "__main__":
+    df['pdocs'] = apply_by_multiprocessing(df, preprocess_text, axis=1, workers=8)
+pdocs = df['pdocs'].values.tolist()
+del df['pdocs']
 dictionary = Dictionary(pdocs)
 print("Dictionary found {} unique tokens".format(len(dictionary)))
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -64,17 +99,22 @@ dictionary.filter_extremes(no_below=10, no_above=0.9)
 dictionary.compactify()
 dictionary.save(r"/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/dictionary.model")
 
-elapsed = datetime.now() - start
+# Write out autocomplete
+with open(r"/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/dictionary_autocomplete.txt", "w+") as tfile:
+    for term in dictionary.values():
+        tfile.write(term)
+        tfile.write(r'\n')
 
+elapsed = datetime.now() - start
 print("Finished Dictionary in {} seconds".format(elapsed.seconds))
 
-
-
 """
+
 VECTORS - Words
+
 """
 
-print("Getting BOW for Occurrence Counts")
+print("Getting Occurrence Counts for Words")
 start = datetime.now()
 bow = list(map(lambda x: [z for z in dictionary.doc2idx(x) if z > -1], pdocs))
 cx = Counter([i for doc in bow for i in doc])
@@ -82,15 +122,9 @@ word_sums = sum(cx.values())
 probabilities = np.array(list(cx.values()))
 probabilities = probabilities / word_sums
 cxp = {k: v for k, v in zip(list(cx.keys()), probabilities)}
-del bow, pdocs
-
-df = pd.read_csv(r"/home/eric/PycharmProjects/FlaskAPI/scripts2/corpus.csv")
-df.dropna(subset=['summary'], inplace=True)
-docs = df['summary'].values.tolist()
-del df
-
 elapsed = datetime.now() - start
 print("Bow Complete in {}".format(elapsed.seconds))
+
 
 def stream_pairs(docs=docs):
     for doc in docs:
@@ -107,22 +141,20 @@ def stream_pairs(docs=docs):
                 for x, y in map(sorted, combinations(w, 2)):
                     yield x, y
 
+
+# Rerun preprocessing to preserve sentence structure
+
 print("Starting Pair Stream")
 start = datetime.now()
 pair_stream = stream_pairs()
 cxy = Counter(pair_stream)
-
 elapsed = datetime.now() - start
 print("Got cxy values in {} seconds".format(elapsed.seconds))
 pair_sums = sum(cxy.values())
-
 probabilities = np.array(list(cxy.values()))
 probabilities = probabilities / pair_sums
-
 cxyp = {k: v for k, v in zip(list(cxy.keys()), probabilities)}
-
 print('Building Sparse PMI Matrix')
-
 pmi_samples = Counter()
 data, rows, cols = [], [], []
 for (x, y), n in cxy.items():
@@ -131,27 +163,56 @@ for (x, y), n in cxy.items():
     data.append(log((n / pair_sums) / (cx[x] / word_sums) / (cx[y] / word_sums)))
     pmi_samples[(x, y)] = data[-1]
 PMI = csc_matrix((data, (rows, cols)))
-
 del data, rows, cols, probabilities
-
 U, _, _ = svds(PMI, k=100)
 norms = np.sqrt(np.sum(np.square(U), axis=1, keepdims=True))
 U /= np.maximum(norms, 1e-9)
-
 np.save("/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/lda_pmi.npy", U)
+del U, norms, pmi_samples
 
-del U,norms, pmi_samples, probabilities, pair_sums, docs, pdocs, dictionary
+"""
 
+Words - Fingerprinting
+
+"""
+
+print("Starting Fingerprint")
+start = datetime.now()
+cnt = TfidfVectorizer(use_idf=True, sublinear_tf=True)
+X = cnt.fit_transform(pdocs)
+X_grp1 = X
+doc_freqs_grp1 = np.sum(X_grp1, axis=0)
+doc_freqs_grp1 = np.array(doc_freqs_grp1)[0]
+# Calculate probabilities
+doc_probs_grp1 = doc_freqs_grp1 / X_grp1.shape[0]
+np.save(arr=doc_probs_grp1,
+        file=r"/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/fingerprint_pop_occ.npy")
+
+elapsed = datetime.now() - start
+
+with open(r"/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/fingerprint_vec.pkl", 'wb+') as pfile:
+    pickle.dump(cnt, pfile)
+print("Finished Fingerprint in {}".format(elapsed.seconds))
+
+del df, cnt, pdocs, X, X_grp1, doc_freqs_grp1, doc_probs_grp1, PMI, cxy, cx, cxyp, cxp
 
 """
 
 SKILLS
 
 """
-df = pd.read_csv(r"/home/eric/PycharmProjects/FlaskAPI/scripts2/corpus.csv")
-df.dropna(subset=['skills'], inplace=True)
-skills = df['skills'].values.tolist()
-del df
+if __name__ == "__main__":
+    df = pd.read_csv(r"/home/eric/PycharmProjects/FlaskAPI/scripts2/corpus.csv")
+    df.dropna(subset=['skills'], inplace=True)
+    start = datetime.now()
+    print("Starting Language Detection for Skills")
+    df['lang'] = apply_by_multiprocessing(df, lang_detect, axis=1, workers=8)
+    df = df.loc[df['lang'] == 'en']
+    elapsed = datetime.now() - start
+
+    print("Finished Language Detection in {} minutes".format(elapsed.seconds // 60))
+
+docs = df['skills'].values.tolist()
 
 
 def preprocess_skills(x):
@@ -163,14 +224,19 @@ DICTIONARY - Skills
 """
 print("Building Skills Dictionary")
 start = datetime.now()
-pdocs = [preprocess_skills(doc) for doc in skills]
-del skills
+pdocs = [preprocess_skills(doc) for doc in docs]
 dictionary = Dictionary(pdocs)
 print("Dictionary found {} unique tokens".format(len(dictionary)))
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 dictionary.filter_extremes(no_below=10, no_above=0.9)
 dictionary.compactify()
 dictionary.save(r"/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/dictionary_skills.model")
+
+# Write out autocomplete
+with open(r"/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/dictionary_skills_autocomplete.txt",
+          "w+") as tfile:
+    for term in dictionary.values():
+        tfile.write(term)
+        tfile.write(r'\n')
 
 elapsed = datetime.now() - start
 print("Finished Skills Dictionary in {} seconds".format(elapsed.seconds))
@@ -179,7 +245,7 @@ print("Finished Skills Dictionary in {} seconds".format(elapsed.seconds))
 VECTORS - Skills
 """
 
-print("Getting BOW for Skills Occurrence Counts")
+print("Getting Skills Occurrence Counts")
 start = datetime.now()
 bow = list(map(lambda x: [z for z in dictionary.doc2idx(x) if z > -1], pdocs))
 cx = Counter([i for doc in bow for i in doc])
@@ -187,9 +253,9 @@ word_sums = sum(cx.values())
 probabilities = np.array(list(cx.values()))
 probabilities = probabilities / word_sums
 cxp = {k: v for k, v in zip(list(cx.keys()), probabilities)}
-
 elapsed = datetime.now() - start
 print("Bow Complete in {}".format(elapsed.seconds))
+
 
 def stream_skills(docs=pdocs):
     for skill_grp in docs:
@@ -207,18 +273,11 @@ print("Starting Skills Pair Stream")
 start = datetime.now()
 pair_stream = stream_skills()
 cxy = Counter(pair_stream)
-
-elapsed = datetime.now() - start
-print("Got cxy values in {} seconds".format(elapsed.seconds))
 pair_sums = sum(cxy.values())
-
 probabilities = np.array(list(cxy.values()))
 probabilities = probabilities / pair_sums
-
 cxyp = {k: v for k, v in zip(list(cxy.keys()), probabilities)}
-
 print('Building Sparse PMI Matrix for Skills')
-
 pmi_samples = Counter()
 data, rows, cols = [], [], []
 for (x, y), n in cxy.items():
@@ -227,7 +286,6 @@ for (x, y), n in cxy.items():
     data.append(log((n / pair_sums) / (cx[x] / word_sums) / (cx[y] / word_sums)))
     pmi_samples[(x, y)] = data[-1]
 PMI = csc_matrix((data, (rows, cols)))
-
 del data, rows, cols, probabilities
 
 U, _, _ = svds(PMI, k=100)
@@ -235,4 +293,52 @@ norms = np.sqrt(np.sum(np.square(U), axis=1, keepdims=True))
 U /= np.maximum(norms, 1e-9)
 
 np.save("/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/lda_pmi_skills.npy", U)
+elapsed = datetime.now() - start
+print("Finished Skills Vec in {}".format(elapsed.seconds))
 
+"""
+Phraser
+"""
+
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(r"/home/eric/PycharmProjects/FlaskAPI/scripts2/corpus.csv")
+    df.dropna(subset=['summary'], inplace=True)
+    start = datetime.now()
+    print("Starting Language Detection for Phrases")
+    df['lang'] = apply_by_multiprocessing(df, lang_detect, axis=1, workers=8)
+    df = df.loc[df['lang'] == 'en']
+    elapsed = datetime.now() - start
+
+    print("Finished Language Detection in {} seconds".format(elapsed.seconds))
+
+docs = df['summary'].values.tolist()
+
+def stream_phrases(docs=docs, stopwords=STOPWORDS):
+    from pattern.en import parsetree
+    punc = set(string.punctuation)
+    for doc in docs:
+        tree = parsetree(x, tokenize=True, lemmata=True)
+        doc = [sent.lemma for sent in tree]
+        for sent in doc:
+            if not sent:
+                continue
+            psent = [word for word in sent if word not in punc]
+            if not sent:
+                continue
+            psent = [word for word in psent if word not in stopwords]
+            if not sent:
+                continue
+            if len(psent) < 3:
+                continue
+            yield psent
+
+phrase_stream = stream_phrases()
+print("Starting Phrase Stream")
+start = datetime.now()
+phrase_model = Phrases(phrase_stream)
+phrase = Phraser(phrase_model)
+phrase.save(r"/home/eric/PycharmProjects/Percy/mysite/app_folder/resources/phrases.model")
+elapsed = datetime.now() - start
+print("Finished Phrase Model in {} minutes".format(elapsed.seconds // 60))
