25f513eb4929d59fd25be3b39c6c937017ff1da2
estasney <16440579+estasney@users.noreply.github.com>
2018-08-14 12:03:27 -0400

    fixes

--- mysite/app_folder/api/routes.py
+++ mysite/app_folder/api/routes.py
@@ -10,19 +10,19 @@ def listen_webhook():
     data_id = hook_data['data']['id']  # The message id
     room_type = hook_data['data']['roomType']  # direct or group
 
+    print(hook_data)
+
     message_details = request_message_details(data_id)
+
+    print(message_details)
 
     intent_parser = SynonymParser()
 
-    if message_details['message_body'] not in intent_parser:
-        # Unable to match query
-        return ""
-
     answer = intent_parser.answer_question(message_details['message_body'])
     if not answer:
-        return
+        answer = "Sorry {}, I didn't understand that".format(message_details['person_fname'])
     else:
         answer = "Hi {}, {}".format(message_details['person_fname'], answer)
-        make_reply(answer, room_type, message_details)
+    make_reply(answer, room_type, message_details)
     return ""
 
