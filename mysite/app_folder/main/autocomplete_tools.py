0812d9f8c07add73642bfc43aa9a655ca099dc63
estasney <16440579+estasney@users.noreply.github.com>
2019-01-22 14:22:37 -0500

    backend

--- mysite/app_folder/main/autocomplete_tools.py
+++ mysite/app_folder/main/autocomplete_tools.py
@@ -1,12 +1,17 @@
 from app_folder.site_config import FConfig
 
 
-def get_autocomplete(dataset, words=FConfig.dictionary_autocomplete, skills=FConfig.dictionary_skills_autocomplete):
+def get_autocomplete(dataset, words=FConfig.dictionary_autocomplete, skills=FConfig.dictionary_skills_autocomplete,
+                     names=FConfig.names_autocomplete):
 
     if dataset == 'words':
         with open(words, 'r') as tfile:
             d1 = tfile.read().splitlines()
             return d1
+    elif dataset == 'names':
+        with open(names, 'r') as tfile:
+            names = tfile.read().splitlines()
+            return names
     else:
         with open(skills, 'r') as tfile:
             d2 = tfile.read().splitlines()
