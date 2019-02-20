ac1bbfead7fd4aa49250553b1189d3f11ea23d4e
estasney <16440579+estasney@users.noreply.github.com>
2019-02-20 13:02:53 -0500

    fix form

--- mysite/app_folder/main/autocomplete_tools.py
+++ mysite/app_folder/main/autocomplete_tools.py
@@ -2,7 +2,7 @@ from app_folder.site_config import FConfig
 
 
 def get_autocomplete(dataset, words=FConfig.dictionary_autocomplete, skills=FConfig.dictionary_skills_autocomplete,
-                     names=FConfig.names_autocomplete):
+                     ):
 
     if dataset == 'words':
         with open(words, 'r') as tfile:
