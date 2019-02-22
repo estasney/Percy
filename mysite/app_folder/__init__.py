e4296f0416b48df2a22282e50f0b90db75d0a5bc
estasney <16440579+estasney@users.noreply.github.com>
2019-02-22 12:07:39 -0500

    typo add moment

--- mysite/app_folder/__init__.py
+++ mysite/app_folder/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 from app_folder.site_config import Config, FConfig
+from flask_moment import Moment
 
 toolbar = DebugToolbarExtension()
+moment = Moment()
 
 
 def create_app(config_class=Config):
     app_run = Flask(__name__)
     app_run.config.from_object(config_class)
+
     toolbar.init_app(app_run)
+    moment.init_app(app_run)
 
     from app_folder.main import bp as main_bp
     app_run.register_blueprint(main_bp)
