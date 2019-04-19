de6fc735eda4466673abc8c5ded8bee727736149
estasney <16440579+estasney@users.noreply.github.com>
2019-04-19 14:03:14 -0400

    encodings

--- mysite/app_folder/main/autocomplete_tools.py
+++ mysite/app_folder/main/autocomplete_tools.py
@@ -6,12 +6,12 @@ fconfig = FConfig()
 def get_autocomplete(dataset, words=fconfig.AUTOCOMPLETE_TOKENS, skills=fconfig.AUTOCOMPLETE_SKILLS):
 
     if dataset == 'words':
-        with open(words, 'r') as tfile:
+        with open(words, 'r', encoding="utf-8") as tfile:
             d1 = tfile.read().splitlines()
             return d1
 
     else:
-        with open(skills, 'r') as tfile:
+        with open(skills, 'r', encoding="utf-8") as tfile:
             d2 = tfile.read().splitlines()
             return d2
 
