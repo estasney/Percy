bd28a238a8e45cb940f669ef2ea975b4fc93c3f3
estasney <estasney@users.noreply.github.com>
2020-07-06 10:17:07 -0400

    path names

--- mysite/app_folder/flask_packer/__init__.py
+++ mysite/app_folder/flask_packer/__init__.py
@@ -1,73 +1,72 @@
 import os
 import fnmatch
-
 from flask import current_app, url_for
-import json
 
 
 class Packer(object):
-    MANIFEST_NAME = ".packer_manifest.json"
 
     def __init__(self, app=None):
         self.app = app
-        self.static_folder = None
-        self.static_manifest_path = None
+        self.static_folder_abs = None
+        self.static_folder_name = None
         self.asset_manifest = {}
+        self.debug = None
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
-        self.static_folder = os.path.basename(app.static_folder)
-        self._build_manifest_path()
+
+        # app.static_folder_abs is an absolute path
+        self.static_folder_abs = app.static_folder
+        self.static_folder_name = os.path.dirname(app.static_folder)
+        self.debug = app.config.get("DEBUG", False)
         app.add_template_global(self.asset_url_for)
 
-    def _build_manifest_path(self):
-        self.static_manifest_path = os.path.join(self.static_folder, self.MANIFEST_NAME)
-
     def _build_manifest(self):
+
+        """
+        Builds a dictionary where
+
+        {somefile.ext : somefile.hash.ext}
+
+        """
         from flask import current_app
-        current_app.logger.info("Build Manifest")
         manifest = {}
         ignored = set(current_app.config.get("PACKER_IGNORE", set([])))
-        ignored.add("**/{}".format(Packer.MANIFEST_NAME))
-        for root_dir, folders, files in os.walk(self.static_folder):
-            root_dir = os.path.relpath(root_dir, "static")
+
+        # We only include path info after the dirname of self.static_folder_abs
+
+        for root_dir, folders, files in os.walk(self.static_folder_abs):
             for file in files:
-                full_path = os.path.join(root_dir, file)
+                full_path = os.path.join(root_dir, file)  # full path is used for fnmatch
                 if any([fnmatch.fnmatch(full_path, i) for i in ignored]):
                     continue
+
+                # get the relative path from self.static_folder_abs
+                root_rel, filename = os.path.split(os.path.relpath(full_path, self.static_folder_abs))
+
                 try:
-                    filename, file_hash, *file_ext = file.split(".")
+                    filename, file_hash, *file_ext = filename.split(".")
                 except ValueError:
                     continue
                 file_ext = ".".join(file_ext)
-                common_name = os.path.join(root_dir, "{}.{}".format(filename, file_ext))
+                common_name = os.path.join(root_rel, "{}.{}".format(filename, file_ext))
                 if common_name in manifest:  # keep the newest
-                    selected = max(
-                            [manifest[common_name],
-                             os.path.join(root_dir, file)],
-                            key=lambda x: os.path.getmtime(x))
-                    manifest[common_name] = selected
+                    current_app.logger.warning("Duplicate File Matched for {}".format(common_name))
                 else:
-                    manifest[common_name] = os.path.join(root_dir, file)
+                    manifest[common_name] = os.path.join(root_rel, file)
 
-        with open(self.static_manifest_path, "w+") as fp:
-            json.dump(manifest, fp)
+        if self.debug:
+            current_app.logger.info("Built Manifest with {} items".format(len(manifest)))
         self.asset_manifest = manifest
 
-    def _load_manifest(self):
-        if not self.static_manifest_path:
-            self._build_manifest_path()
-        if not os.path.isfile(self.static_manifest_path):
-            self._build_manifest()
-        with open(self.static_manifest_path, "r") as fp:
-            self.asset_manifest = json.load(fp)
-
     def asset_url_for(self, folder, filename):
-
+        from flask import current_app
         if not self.asset_manifest:
-            current_app.logger.info("Load Manifest")
-            self._load_manifest()
+            self._build_manifest()
         manifest_path = self.asset_manifest.get(filename, filename)
-        current_app.logger.info("Asset URL For {}, Found {}".format(filename, manifest_path))
+        if manifest_path == filename:
+            current_app.logger.info("No hashed asset found for {}".format(filename))
+        if self.debug:
+            current_app.logger.info("Asset URL For {}, Found {}".format(filename, manifest_path))
         return url_for(folder, filename=manifest_path)
